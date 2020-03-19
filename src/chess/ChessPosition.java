b4cc9697643774a452fbd92f6a443028867a545f
JulioMaiaB <julio_maia1@hotmail.com>
2020-03-18 23:24:20 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,44 @@
+package chess;
+
+import boardgame.Position;
+
+// Classe que determina a posição na classe de xadrez. Exemplo: a1, d5, h4, etc.
+public class ChessPosition {
+	
+	private char column;
+	private int row;
+	
+	public ChessPosition(char column, int row) {
+		// Programação defensiva
+		if (column < 'a' || column > 'h' || row < 1 || row > 8 ) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	// Converte a posição do xadrez (ChessPosition) para a posição normal (Position)
+	protected Position toPosition() {
+		// Foi utilizado o conceito de UNICODE para fazer as comparações entre variaveis do tipo char.
+		return new Position(8 - row, column /*que é um char*/ - 'a');
+	}
+	
+	// Converte a posição normal (Position)  para a posição do xadrez (ChessPosition)
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	// Foi necessário utilizar um macete utilizando uma concatenação de String vazia "" para o compilador entender que se trata de uma string
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+}
