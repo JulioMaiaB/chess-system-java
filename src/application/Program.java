86af3ad76094e805fc19b4b515f49285a5a5af3a
JulioMaiaB <julio_maia1@hotmail.com>
2020-03-30 01:30:36 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,11 +16,13 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
 		
 		while(true) {
 			try {		
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -32,6 +36,10 @@ public class Program {
 				
 				// O  vai ser feito com essa peça capturada ? Cenas do próximo capítulo
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if(capturedPiece != null) { // significa que alguma peça foi capturada
+					captured.add(capturedPiece); // Adiciona na lista de peças capturadas
+				}
 			}
 			catch(ChessException e) {
 				System.out.println(e.getMessage());
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,7 +14,12 @@ public class ChessMatch {
 	
 	private int turn; // Contagem de turno da partida
 	private Color currentPlayer; // Indica se a vez de jogar é da cor branca ou preta
-	private Board board; // Partida de xadrez precisa ter um tabuleiro 
+	private Board board; // Partida de xadrez precisa ter um tabuleiro
+	
+	// Lista para controlar as peças que estão no tabuleiro e as peças que foram capturadas
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
+
 	
 	public ChessMatch() {
 		// Importante entender: quem precisar saber a dimensão do jogo de xadrez é a classe ChessMatch
@@ -66,6 +74,12 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if(capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
+		
 		return capturedPiece;
 	}
 	
@@ -94,6 +108,7 @@ public class ChessMatch {
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 	
 	// ChessPiece piece = new Rook(board, Color.WHITE));
